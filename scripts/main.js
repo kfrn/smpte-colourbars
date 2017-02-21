7bc29d5a0a848c7841b16fa66c6eb41e08c866c0
kfrn <knfrances@gmail.com>
2017-02-21 21:47:40 +1300

    SMPTE colourbars in progress

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+/* COLOURS */
+
 const mainColours = {
   white: "white",
   yellow: "#c4c400",
@@ -9,9 +11,13 @@ const mainColours = {
   black: "black"
 }
 
-const colourValues = Object.keys(mainColours)
-                           .map(key => mainColours[key])
+const colourValues = Object.keys(mainColours).map(key => mainColours[key])
 
+const SMPTEcolours = colourValues.slice()
+SMPTEcolours[0] = "lightgrey"
+
+
+/* FUNCTIONS */
 
 function drawEBUColourBars(x, y, width, height) {
   const svg = d3.select("#ebu-cb")
@@ -34,6 +40,7 @@ function drawEBUColourBars(x, y, width, height) {
   })
 }
 
+
 function drawSMPTEColourBars(x, y, width, height) {
   const svg = d3.select("#smpte-cb")
   const g = svg.append("g")
@@ -44,7 +51,34 @@ function drawSMPTEColourBars(x, y, width, height) {
       .attr("width", width)
       .attr("height", height)
       .attr("fill", "lightblue")
+
+  // Standard EIA 75% amplitude white bars (67% of frame height)
+  d3.range(8).forEach((d, i) => {
+    g.append("rect")
+    .attr("x", x + d * width / 8)
+    .attr("y", y)
+    .attr("width", width / 8)
+    .attr("height", height * 0.67)
+    .attr("fill", SMPTEcolours[i])
+  })
+
+  // Reverse blue bars (8% of frame height)
+  g.append("rect")
+      .attr("x", x)
+      .attr("y", y + height * 0.67)
+      .attr("width", width)
+      .attr("height", height * 0.08)
+      .attr("fill", "blue")
+
+  // PLUGE signal (25% of frame height)
+  g.append("rect")
+      .attr("x", x)
+      .attr("y", y + height * 0.75)
+      .attr("width", width)
+      .attr("height", height * 0.25)
+      .attr("fill", "black")
 }
+
 
 function drawHDColourBars(x, y, width, height) {
   const svg = d3.select("#smpte-cb-hd")
@@ -58,6 +92,8 @@ function drawHDColourBars(x, y, width, height) {
       .attr("fill", "lightblue")
 }
 
+
+/* CALL FUNCTIONS */
 
 drawEBUColourBars(0, 0, 768, 576)
 drawSMPTEColourBars(0, 0, 768, 576)
